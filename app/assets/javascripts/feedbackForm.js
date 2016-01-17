131e7da13cb0a2b7e7cc3280ae2fed72de8d5147
Matthew Dick <matthew@jaxkneppers.com>
2016-01-16 17:36:38 -0800

    autocomplete working for content and peer tagging

--- app/assets/javascripts/feedbackForm.js
+++ app/assets/javascripts/feedbackForm.js
@@ -2,19 +2,18 @@ $(document).ready(function(){
 
 
     var feedbackContent = $('.feedback-form #feedback_content'),
-        recipients = [],
-        peers = $('.feedback-form #peers');
-
-    function split( val ) {
-        return val.split( /,\s*/ );
-    }
-    function extractLast( term ) {
-        return split( term ).pop();
-    }
+        recipientList = [],
+        peers = $('.feedback-form #peers'),
+        peerList = [];
 
     $.get('/recipients', function(data) {
         $.each(data, function (i, user) {
-            recipients.push(user);
+            recipientList.push(user);
+        })
+    });
+    $.get('/peers', function(data) {
+        $.each(data, function (i, user) {
+            peerList.push(user);
         })
     });
 
@@ -29,7 +28,7 @@ $(document).ready(function(){
         .autocomplete({
             source: function( request, response ) {
                 var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(request.term), "i");
-                response( $.grep( recipients, function( item ){
+                response( $.grep( recipientList, function( item ){
                     return matcher.test( item.user_tag );
                 }))
             },
@@ -39,14 +38,7 @@ $(document).ready(function(){
                 return false;
             },
             select: function( event, ui ) {
-                var terms = split( this.value );
-                // remove the current input
-                terms.pop();
-                // add the selected item
-                terms.push( ui.item.value );
-                // add placeholder to get the comma-and-space at the end
-                terms.push( "" );
-                this.user_tag = terms.join( ", " );
+                $(this).val(ui.item.user_tag + ' ');
                 return false;
             }
         })
@@ -57,49 +49,60 @@ $(document).ready(function(){
         };
 
 
-//    var recipients = new Bloodhound({
-//        datumTokenizer: Bloodhound.tokenizers.whitespace,
-//        queryTokenizer: Bloodhound.tokenizers.whitespace,
-//        remote: {
-//            url: '/recipients'
-//        }
-//    });
-//
-//    var peers = new Bloodhound({
-//        datumTokenizer: Bloodhound.tokenizers.whitespace,
-//        queryTokenizer: Bloodhound.tokenizers.whitespace,
-//        remote: {
-//            url: '/peers'
-//        }
-//    });
-//
-//    $('#feedback_content').typeahead({
-//        hint: true,
-//        highlight: true,
-//        minLength: 1
-//    }, {
-//        name: 'recipients',
-//        display: 'user_tag',
-//        source: recipients,
-//        limit: 10,
-//        templates: {
-//            suggestion: Handlebars.compile('<div><img src="{{avatar_url}}"><div class="info"><div class="name">{{name}}</div><div class="title">{{title}}</div><div class="user_tag">{{user_tag}}</div></div></div>')
-//        }
-//    });
-//
-//    $('#peers').typeahead({
-//        hint: true,
-//        highlight: true,
-//        minLength: 1
-//    }, {
-//        name: 'peers',
-//        display: 'user_tag',
-//        source: peers,
-//        limit: 10,
-//        templates: {
-//            suggestion: Handlebars.compile('<div><img src="{{avatar_url}}"><div class="info"><div class="name">{{name}}</div><div class="title">{{title}}</div><div class="user_tag">{{user_tag}}</div></div></div>')
-//        }
-//    });
+    function split( val ) {
+        return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+        return split( term ).pop();
+    }
+
+    peers
+        .bind( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                $( this ).autocomplete( "instance" ).menu.active ) {
+                event.preventDefault();
+            }
+        })
+        .autocomplete({
+            source: function( request, response ) {
+                var term = request.term;
+
+                // substring of new string (only when a comma is in string)
+                if (term.indexOf(', ') > 0) {
+                    var index = term.lastIndexOf(', ');
+                    term = term.substring(index + 2);
+                }
+
+                // regex to match string entered with start of suggestion strings
+                var re = $.ui.autocomplete.escapeRegex(term);
+                var matcher = new RegExp('^' + re, 'i');
+
+                var regex_validated_array = $.grep(peerList, function (user) {
+                    return matcher.test(user.user_tag);
+                });
+
+                response(regex_validated_array);
+            },
+            minLength: 1,
+            focus: function() {
+                // prevent value inserted on focus
+                return false;
+            },
+            select: function( event, ui ) {
+                var terms = split(this.value);
+                terms.pop();
+                terms.push(ui.item.user_tag);
+                terms.push('');
+                this.value = terms.join(', ');
+                return false;
+            }
+        })
+        .autocomplete( "instance" )._renderItem = function( ul, item ) {
+        return $( "<li>" )
+            .append( "<div class='avatar'><img src='" + item.avatar_url + "'/></div><div class='info'>" + item.name + "<div class='title'>" + item.title + "</div>" + item.user_tag + "</div>" )
+            .appendTo( ul );
+    };
+
 
     $('.feedback-form .submit-tag').click(function(){
         $(this).closest('form').submit();
