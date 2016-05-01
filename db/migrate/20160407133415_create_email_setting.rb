138bfd3c86ee7f771db673e0d61a20d816206907
Anthony DeCino <adecino@riotgames.com>
2016-04-30 23:54:01 -0700

    send feedback/comment bug fix

--- db/migrate/20160407133415_create_email_setting.rb
+++ db/migrate/20160407133415_create_email_setting.rb
@@ -7,5 +7,12 @@ class CreateEmailSetting < ActiveRecord::Migration
 
       t.timestamps
     end
+    User.all.each do |u|
+	  MailerSetting::DEFAULT_SETTINGS.each do |m|
+	    mail = MailerSetting.new(m)
+	    mail.user = u
+	    mail.save
+	  end
+	end
   end
 end
