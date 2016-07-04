f9ec590dcc64118245698e7870b766afde7187aa
Matthew Dick <mattwd7@gmail.com>
2016-07-04 15:39:39 -0700

    mailer heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.initialize_on_precompile = false
+  config.action_mailer.default_url_options = { :host => 'prophet1.herokuapp.com' }
 end
