92e060e100cd1464cb417b0e9fb93e00c9d0e5fc
Matthew Dick <mattwd7@gmail.com>
2016-07-26 07:21:36 -0700

    mailer settings in productin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'prophet.mailer1@gmail.com',
+      :password             => 'WeAreProphet',
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
   # config.action_mailer.default :charset => "utf-8"
 end
